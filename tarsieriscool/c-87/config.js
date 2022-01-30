720eda0e91af73b08cbf4e9e31cc91a8ac1df319
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2022-01-30 19:09:30 +0530

    Add files via upload

---
+++ tarsieriscool/c-87/config.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBphA_jHSphNdKp6WVGmE1kyxSdP7yR5pE",
+  authDomain: "story-telling-app-3c312.firebaseapp.com",
+  projectId: "story-telling-app-3c312",
+  storageBucket: "story-telling-app-3c312.appspot.com",
+  messagingSenderId: "843788254365",
+  appId: "1:843788254365:web:1cd3d79c1b080be43a6874"
+};
+
